6a1f8f044c2b1263e988981b43af5fe64cdf9f1f
Danny Roberts <daniel.mattos.roberts@gmail.com>
2016-10-31 00:26:35 -0400

    jspm install --dev npm:babel-plugin-transform-react-jsx

--- jspm.config.js
+++ jspm.config.js
@@ -8,7 +8,39 @@ SystemJS.config({
   },
   devConfig: {
     "map": {
-      "plugin-babel": "npm:systemjs-plugin-babel@0.0.17"
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.17",
+      "babel-plugin-transform-react-jsx": "npm:babel-plugin-transform-react-jsx@6.8.0"
+    },
+    "packages": {
+      "npm:babel-plugin-transform-react-jsx@6.8.0": {
+        "map": {
+          "babel-runtime": "npm:babel-runtime@6.18.0",
+          "babel-plugin-syntax-jsx": "npm:babel-plugin-syntax-jsx@6.18.0",
+          "babel-helper-builder-react-jsx": "npm:babel-helper-builder-react-jsx@6.18.0"
+        }
+      },
+      "npm:babel-runtime@6.18.0": {
+        "map": {
+          "core-js": "npm:core-js@2.4.1",
+          "regenerator-runtime": "npm:regenerator-runtime@0.9.5"
+        }
+      },
+      "npm:babel-helper-builder-react-jsx@6.18.0": {
+        "map": {
+          "babel-runtime": "npm:babel-runtime@6.18.0",
+          "lodash": "npm:lodash@4.16.4",
+          "esutils": "npm:esutils@2.0.2",
+          "babel-types": "npm:babel-types@6.18.0"
+        }
+      },
+      "npm:babel-types@6.18.0": {
+        "map": {
+          "babel-runtime": "npm:babel-runtime@6.18.0",
+          "esutils": "npm:esutils@2.0.2",
+          "lodash": "npm:lodash@4.16.4",
+          "to-fast-properties": "npm:to-fast-properties@1.0.2"
+        }
+      }
     }
   },
   transpiler: "plugin-babel",
