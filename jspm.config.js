fcf8d54d8247d31b900657b720777b5fa1c61545
Danny Roberts <daniel.mattos.roberts@gmail.com>
2016-10-31 00:25:20 -0400

    jspm init .

---
+++ jspm.config.js
@@ -0,0 +1,34 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "es6-demo/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.17"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "es6-demo": {
+      "main": "es6-demo.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
